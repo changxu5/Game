163e42a6a6ad5b9cfa66e7e935696b6a185d5768
maoxukun1994 <1690192590@qq.com>
2015-06-09 08:21:17 +0800

    Create Player.h

--- Player.h
+++ Player.h
@@ -1,11 +1,10 @@
 #pragma once
 
 #include "cocos2d.h"
-#define MOVE_FORCE 200.0
-#define JUMP_IMPULSE 30000.0
-#define SCALEFACTOR 20.0
-#define FIREPOWER_INCREASEMENT 5.0
-#define MAX_FIREPOWER 10000.0
+
+#define ACC 500
+#define MAX_V_SPEED 500
+#define MAX_H_SPEED 200
 
 USING_NS_CC;
 
@@ -18,42 +17,18 @@ public:
 	bool init()
 	{
 		this->onGround = false;
-		this->forced = 0;
 		this->firepower = 0;
 		this->addingPower = false;
+		this->fireable = true;
+		this->velocity = Vec2::ZERO;
+		this->destPos = Vec2::ZERO;
 
 		return true;
 	}
 
 	void update(float dt)
 	{
-		auto body = this->getPhysicsBody();
-		if (this->forced == 1)
-		{
-			body->applyForce(Vec2(MOVE_FORCE,0));
-		}
-		else
-		{
-			if (this->forced == -1)
-			{
-				body->applyForce(Vec2(-MOVE_FORCE, 0));
-			}
-			else
-			{
-				body->resetForces();
-			}
-		}
-		if (this->addingPower == true)
-		{
-			this->firepower += FIREPOWER_INCREASEMENT * SCALEFACTOR;
-			if (this->firepower>MAX_FIREPOWER)
-			{
-				this->fire();
-				this->addingPower = false;
-			}
-			log("Firepower : %f", firepower);
-		}
-
+		this->calculateMovement(dt);
 	}
 
 	void setSprite(Sprite *a)
@@ -68,40 +43,69 @@ public:
 	}
 
 	void jump()
-	{
-		this->getPhysicsBody()->applyImpulse(Vec2(0,JUMP_IMPULSE));
+	{ 
+		if (!onGround) return;
+		this->velocity = velocity + Vec2(0,400);
+		this->setOnGround(false);
 	}
 
 	void fire()
 	{
 		this->addingPower = false;
-		//
-		//
-		auto newBullet = Sprite::create("bullet.png");
-		auto bulletBody = PhysicsBody::createBox(newBullet->getContentSize());
-		bulletBody->setMass(20.0f);
-		bulletBody->setCategoryBitmask(0xFFFFFFF0);
-		bulletBody->setContactTestBitmask(0x00000001);
-		newBullet->setPhysicsBody(bulletBody);
-		newBullet->setTag(2);
-		this->addChild(newBullet);
-		newBullet->setPosition(0, 0);
-		newBullet->getPhysicsBody()->applyImpulse(Vec2(firepower / 1.414, firepower / 1.414));
-		this->getPhysicsBody()->applyImpulse(Vec2(-firepower / 1.414, -firepower / 1.414));
-		//
 		this->firepower = 0;
 	}
 
-	void startupboost()
+	Rect getRect()
 	{
-		if (forced == 1) this->getPhysicsBody()->applyImpulse(Vec2(1200.0, 0));
-		if (forced == -1) this->getPhysicsBody()->applyImpulse(Vec2(-1200.0, 0));
+		auto box = spr->getBoundingBox();
+		return Rect(box.getMinX() + this->destPos.x, box.getMinY() + this->destPos.y, spr->getContentSize().width, spr->getContentSize().height);
+	}
+	void calculateMovement(float dt)
+	{
+		velocity.y -= ACC * dt;
+		if (abs(velocity.y) > MAX_V_SPEED)
+		{
+			if (velocity.y > 0) velocity.y = MAX_V_SPEED;
+			else velocity.y = -MAX_V_SPEED;
+		}
+		auto mov = velocity *  dt;
+		this->destPos = this->getPosition() + mov;
+	}
+	void move()
+	{
+		this->setPosition(this->destPos);
+	}
+
+	void setOnGround(bool a)
+	{
+		this->onGround = a;
+		if (a)
+		{
+			this->velocity.y = 0;
+			this->destPos.y = this->getPosition().y;
+		}
+	}
+
+	void setRanintoWall(bool a)
+	{
+		if (a)
+		{
+			this->velocity.x = 0;
+			this->destPos.x = this->getPositionX();
+		}
+	}
+
+	void moveto(int direc)
+	{
+		velocity.x = direc * MAX_H_SPEED;
 	}
 
 	bool onGround;
-	int forced;
 	bool addingPower;
 	float firepower;
+	bool fireable;
+	Vec2 velocity;
+	Vec2 destPos;
 
 private:
 	
